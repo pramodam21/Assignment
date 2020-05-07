e830c90caf368228b9671f22e7d9f043cad11163
pramoda-gcn <pramoda.m@playsportsnetwork.com>
2020-05-07 19:06:26 +0530

    Added Location Component

--- src/Components/Molecules/Description/Description.js
+++ src/Components/Molecules/Description/Description.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Style from "./Style.module.css";
 import HeaderContent from "../DescriptionHeader/HeaderContent";
+import Location from '../Location/Location';
 
 const Description = () => (
   <div className={Style.container}>
     <HeaderContent />
+    <Location />
   </div>
 );
 
