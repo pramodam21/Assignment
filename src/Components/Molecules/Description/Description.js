ea23b849f677da7f44ce40a6bfd103adcc6fc74f
pramoda-gcn <pramoda.m@playsportsnetwork.com>
2020-05-08 13:28:36 +0530

    Added Food comopent

--- src/Components/Molecules/Description/Description.js
+++ src/Components/Molecules/Description/Description.js
@@ -2,11 +2,13 @@ import React from "react";
 import Style from "./Style.module.css";
 import HeaderContent from "../DescriptionHeader/HeaderContent";
 import Location from '../Location/Location';
+import FoodItem from '../FoodItem/FoodItem';
 
 const Description = () => (
   <div className={Style.container}>
     <HeaderContent />
     <Location />
+    <FoodItem />
   </div>
 );
 
